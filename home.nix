139bc1959954ae7e799833a14db36eba71bc939b
dershtal <dershtal@gmail.com>
2025-05-13 23:27:42 +0300

    init-set
    Default setting on vm.

---
+++ home.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, ... }: {
+        home = {
+                username = "dershtal";
+                homeDirectory = "/home/dershtal";
+                stateVersion = "24.11";
+
+                packages = with pkgs; [
+                        neofetch
+                        foot
+                        sox
+                        mc
+                        hyprland
+                        cascadia-code
+                        jetbrains-mono
+                        dejavu_fonts
+                        liberation_ttf
+                        noto-fonts
+                        noto-fonts-cjk-sans
+                        noto-fonts-emoji
+                        wofi
+                        superfile
+                        swaylock
+                ];
+        };
+
+        imports = [
+               ./bash.nix
+        ];
+
+        fonts.fontconfig.enable = true;
+        programs.neovim = {
+                enable = true;
+                defaultEditor = true;
+                plugins = with pkgs.vimPlugins; [
+                        gruvbox-material
+                        nerdtree
+                ];
+        };
+        programs.foot = {
+                enable = true;
+                settings = {
+                  main = {
+                    term = "xterm-256color";
+                    workers = 32;
+                    initial-window-size-chars = "115x24";
+                    pad = "4x4 center";
+                    font = "Cascadia Code PL:size=6";
+                  };
+                };
+        };
+        wayland.windowManager.hyprland = {
+                enable = true;
+                systemd.variables = [ "--all" ];
+                extraConfig = builtins.readFile ./hyprland.conf;
+        };
+}
